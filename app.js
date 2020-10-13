82785ad9f68862824f65d31644fe56f7faab1915
Neelam Chaubey <neelamchaubey28@gmail.com>
2020-10-13 12:46:34 +0530

    without file upload

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 const EmployeeRoute = require('./routes/imageRoutes');
-const path = require('path')
+const path = require('path');
 require("dotenv").config();
 
 const app = express();
